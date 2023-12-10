d5f6cb03f74b3c1cd62ce36258d27109f0ce36bb
Andy Tay <soonwan@gmail.com>
2023-12-11 07:59:20 +0800

    Added api call with authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { NextAuthProvider } from './providers'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,8 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        
-        {children}
+        <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,17 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import axios, { AxiosResponse } from "axios";
 
+import { useSession, getSession, signIn, signOut } from "next-auth/react";
+
 type Inputs = {
   fullName: string;
 };
 
 export default function Page() {
+
+  const { data: session, status } = useSession()
+  
+
   const {
     control,
     handleSubmit,
@@ -30,11 +36,18 @@ export default function Page() {
 
     // const formData = new FormData();
     // formData.append("data", JSON.stringify(data));
+    const session = await getSession();
+
+    console.log(session.accessToken);
+
+    const config = {
+      headers: { Authorization: `Bearer ${session.accessToken}` }
+    };
 
     const response = await axios
       .post(
         "http://localhost:8080/greeting",
-        data,
+        data,config
         )
       .then(function (response) {
         console.log(response);
@@ -48,37 +61,54 @@ export default function Page() {
 
   console.log(watch("fullName")); // watch input value by passing the name of it
 
+  if (status === "loading") {
+    return <p>Hang on there...</p>
+  }
+  
+  if (status === "authenticated") {
+    return (
+      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12}>
+            <button onClick={() => signOut()}>Sign out</button>
+          </Grid>
+          <Grid item xs={12}>
+            <h1>Auth Test App</h1>
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+              name="fullName"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  id="outlined-basic"
+                  label="Full Name"
+                  variant="outlined"
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
+          </Grid>
+          <Grid>
+            <Typography variant="h2">
+              {banner}
+            </Typography>
+          </Grid>
+        </Grid>
+      </form>
+    );
+  }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12}>
-          <h1>Auth Test App</h1>
-        </Grid>
-        <Grid item xs={12}>
-          <Controller
-            name="fullName"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="outlined-basic"
-                label="Full Name"
-                variant="outlined"
-              />
-            )}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" type="submit">
-            Submit
-          </Button>
-        </Grid>
-        <Grid>
-          <Typography variant="h2">
-            {banner}
-          </Typography>
-        </Grid>
-      </Grid>
-    </form>
-  );
+    <>
+      <p>Not signed in.</p>
+      <button onClick={() => signIn("keycloak")}>Sign in</button>
+    </>
+  )
 }
